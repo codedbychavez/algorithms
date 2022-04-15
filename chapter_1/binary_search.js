8feffa55f6c79093227007aed102f810aecba55e
codedbychavez <codedbychavez@gmail.com>
2022-04-15 06:56:46 -0400

    Added binary search in JS

--- chapter_1/binary_search.js
+++ chapter_1/binary_search.js
@@ -1,1 +1,28 @@
-const low = 0;
+
+function binary_search(list, item) {
+  let low = 0
+  let high = list.length - 1
+
+  while(low <= high) {
+    mid = low + high
+    guess = list[mid]
+    if(guess == item) {
+      return mid
+    }
+    if(guess > item) {
+      high = mid - 1
+    }
+    else {
+      low = mid + 1
+    }
+  }
+  return null;
+}
+
+const my_list = [1, 3, 5, 7, 9]
+
+const ans1 = binary_search(my_list, 3)
+const ans2 = binary_search(my_list, -1)
+
+console.log(ans1)
+console.log(ans2)
