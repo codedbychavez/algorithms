5bfd15945273e93eedd8bae4fd1d1605a394538a
codedbychavez <codedbychavez@gmail.com>
2022-04-16 10:59:34 -0400

    Updated solution

--- chapter_1/binary_search.js
+++ chapter_1/binary_search.js
@@ -4,17 +4,21 @@ function binary_search(list, item) {
   let high = list.length - 1
 
   while(low <= high) {
-    mid = low + high
+    mid = (low + high) / 2
+
     guess = list[mid]
+  
     if(guess == item) {
       return mid
     }
-    if(guess > item) {
+
+    if (guess > item) {
       high = mid - 1
     }
     else {
       low = mid + 1
     }
+
   }
   return null;
 }
@@ -22,7 +26,7 @@ function binary_search(list, item) {
 const my_list = [1, 3, 5, 7, 9]
 
 const ans1 = binary_search(my_list, 3)
-const ans2 = binary_search(my_list, -1)
+// const ans2 = binary_search(my_list, -1)
 
-console.log(ans1)
-console.log(ans2)
+console.log('The Answer is: ', ans1)
+// console.log(ans2)
